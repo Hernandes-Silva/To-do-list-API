a02a0bfba5d7c4f884c288e371fa4b9875545048
Hernandes <lertanisslobr@gmail.com>
2021-10-01 14:48:57 -0300

    adcionando o user as tasks

--- tasks/models.py
+++ tasks/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from tasks.generator_slug import unique_slug_generator
+from django.contrib.auth.models import User
 # Create your models here.
 class Task(models.Model):
     status_choice = [('COM', 'Complete'), ('INP', "In progress"), ('TOD', 'Todo')]
     name = models.CharField(max_length=250)
     slug = models.SlugField(max_length = 250, null = True, blank = True, unique=True)
     status = models.CharField(max_length=3, choices=status_choice)
-
+    user = models.ForeignKey(User, related_name='tasks', verbose_name='Usuário', on_delete=models.CASCADE)
     def __str__(self):
        return self.name
 
