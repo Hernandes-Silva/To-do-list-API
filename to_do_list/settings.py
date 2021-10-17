5a47e45260c7e71b932b90a04d874a767793d7dc
Hernandes <lertanisslobr@gmail.com>
2021-10-17 09:09:20 -0300

    Deploy da aplicação no heroku

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 from datetime import timedelta
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+#heroku
+django_heroku.settings(locals())
